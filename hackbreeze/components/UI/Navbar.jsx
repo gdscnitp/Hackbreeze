3461de4cb0f6feec1d761f3377eb197d65e7c09d
sanya <sanya567kumari@gmail.com>
2023-03-13 18:06:47 +0530

    added navbar

--- hackbreeze/components/UI/Navbar.jsx
+++ hackbreeze/components/UI/Navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import '../navbar.css';
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (<nav className="nav">
+  <a href="#" className="brand"> HackerByte</a>
+  <div className="navigation_menu">
+  <ul>
+      <li className="nav_item"><a href="#" className="nav_link">Practice</a></li>
+      <li className="nav_item"><a href="#" className="nav_link">Compete</a></li>
+      <li className="nav_item"><a href="#" className="nav_link">Learn</a></li>
+      <div className="vl">
+      <li className="nav_item"><a href="#" className="nav_link1">Contact us</a></li></div>
+
+  </ul>
+  </div>
+  
+</nav>
+)
 };
 
 export default Navbar;
